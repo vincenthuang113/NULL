e9960dafc88249ba649f0ffa0dd76fbed76f8721
vincenthuang113 <vincenthuang113@gmail.com>
2023-02-10 12:25:00 -0500

    new2

--- src/pages/binanceInternal.jsx
+++ src/pages/binanceInternal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../css/general.css'
-import nullBinance from '../images/nullBinance.jpg';
+// import nullBinance from '../images/nullBinance.jpg';
 
 function BIT(){
     
@@ -10,7 +10,7 @@ function BIT(){
                 <div className="internalT">
                     <h1>請掃描QR Code或是使用NULL支付ID: 505740324</h1>
                     <div className="bitimg">
-                    <img src={nullBinance} width="500vw" height="auto" />
+                    {/* <img src={nullBinance} width="500vw" height="auto" /> */}
                     </div>
                     <h1>請掃描付款QR Code並且完成付款程序</h1>
                     <h1><span>請務必保留您的訂單ID</span></h1>
